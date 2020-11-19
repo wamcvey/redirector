9243fa504eef08b02f9b87a5db2b19f6c392f694
William McVey <wam@wamber.net>
2020-11-19 12:14:23 -0500

    Mid-refactor before rethink to use traefik

--- server.py
+++ server.py
@@ -2,13 +2,14 @@
 
 """A basic single purpose redirection server
 """
+import sys
+import logging
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import socketserver
 import ssl
 
 from http import HTTPStatus
 
-PORT=443
 SITE="https://www.choosefi.com"
 
 
@@ -24,10 +25,32 @@ class Handler(BaseHTTPRequestHandler):
     def do_HEAD(self):
         return self.do_GET()
 
-
-with HTTPServer(('', PORT), Handler) as httpd:
+def ssl_wrap_httpd(httpd, keyfile, certfile):
     # Wrap it like you care for it
     httpd.socket = ssl.wrap_socket(
       httpd.socket, keyfile="server.key", certfile="server.cert")
-    print('Listening on port %s' % (PORT))
-    httpd.serve_forever()
+    return httpd
+
+if __name__ == '__main__':
+    import argparse
+
+    cli = argparse.ArgumentParser(description="Do some web redirections")
+    cli.add_argument("--ssl-key", action="store", help="Start ssl and use key")
+    cli.add_argument("--ssl-cert", action="store", help="Start ssl and use cert")
+    cli.add_argument("--port", action="store", type=int, default=8080, help="port to listen on")
+
+    log=logging.getLogger(sys.argv[0])
+
+
+    args=cli.parse_args()
+    if (args.ssl_key and not args.ssl_cert) or (args.ssl_cert and not args.ssl_key):
+        log.error("Both ssl_key and ssl_cert need to be defined if either one is")
+        sys.exit(1)
+
+
+
+    with HTTPServer(('', PORT), Handler) as httpd:
+        if args.ssl_key:
+            ssl_wrap_httpd(httpd, args.ssl_key, args.ssl_cert):
+        print('Listening on port %s' % (PORT))
+        httpd.serve_forever()
